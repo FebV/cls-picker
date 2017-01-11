b4bd4b21f3a97434f084e71ec2cbcb258b88090a
FebV <cn.zhaoyifan@gmail.com>
2017-01-11 17:55:46 +0800

    improve performance about 300%

--- localPicker.js
+++ localPicker.js
@@ -1,20 +1,33 @@
 const pickerController = require('./modules/PickerController');
 const cfg = require('./config');
-const pc = new pickerController(cfg.stu);
-pc.addPickingClass(cfg.cls);
 
 let itv = null;
 let count = 0;
+const pc = new pickerController(cfg.stu);
+pc.addPickingClass(cfg.cls);
+
+
 async function pick() {
-    await pc.loginAuth();
-    const re = await pc.pick();
+    let re = null;
+    try{
+        re = await pc.pick();
+    }
+    catch(e) {
+        console.log('正在尝试登录');
+        await pc.loginAuth();
+    }
     return re;
 }
+
 
 itv = setInterval(async () => {
     const start = Date.now();
     const re = await pick();
     // const result = !/选课失败/.test(JSON.parse(re.res[0].resBody).msg);
+    if(!re) {
+        console.log(`no response`);
+        return;
+    } 
     const result = JSON.parse(re.res[0].resBody).result == 'error';
     if( result ) {
         clearInterval(itv);
